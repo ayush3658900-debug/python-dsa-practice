16c314b1719c50af7a305f562b7611af181e5b4c
ayush3658900-debug <ayush3658900@gmail.com>
2026-03-06 21:41:13 +0530

    Added Valid parentheses problem using stack

---
+++ problem/valid_parentheses.py
@@ -0,0 +1,18 @@
+def isValid(s):
+    stack = []
+    mapping = {')': '(', '}': '{', ']': '['}
+
+    for char in s:
+        if char in mapping:
+            top = stack.pop() if stack else '#'
+            if mapping[char] != top:
+                return False
+        else:
+            stack.append(char)
+
+    return not stack
+
+
+# Test cases
+print(isValid("()[]{}"))  # True
+print(isValid("(]"))      # False
